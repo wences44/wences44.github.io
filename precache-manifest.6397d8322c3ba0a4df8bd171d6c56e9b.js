8fd73a0fb21c9151cee5a90bbef4b41c8b7da5da
Wences Moreau <Ricardo.Wenceslao.Moreau.Moreno@kyndryl.com>
2023-05-29 16:10:41 -0600

    Updates

---
+++ precache-manifest.6397d8322c3ba0a4df8bd171d6c56e9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4d2b4144bb50b2e20122b1000ff188",
+    "url": "/wences44.github.io/index.html"
+  },
+  {
+    "revision": "05e7201f7568a158b048",
+    "url": "/wences44.github.io/static/css/main.fdbb0f34.chunk.css"
+  },
+  {
+    "revision": "a21e43475a3875f627f6",
+    "url": "/wences44.github.io/static/js/2.8a0ff5f3.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/wences44.github.io/static/js/2.8a0ff5f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e7201f7568a158b048",
+    "url": "/wences44.github.io/static/js/main.afad0d64.chunk.js"
+  },
+  {
+    "revision": "0d4c9c9adc875f6628bb",
+    "url": "/wences44.github.io/static/js/runtime-main.cdc8ebf1.js"
+  },
+  {
+    "revision": "297e08cebd872008d5402482b70c8939",
+    "url": "/wences44.github.io/static/media/kyndryl-logo2.297e08ce.svg"
+  }
+]);
